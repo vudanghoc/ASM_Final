00e256cbdc2028a744b52c8f2902547de57ca724
BK098 <buikhang122004@gmail.com>
2024-06-12 00:19:59 +0700

    hot fix getOrder

--- Services/Models/Product/Base/ProductBaseDto.cs
+++ Services/Models/Product/Base/ProductBaseDto.cs
@@ -5,7 +5,7 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
-        public int Stock { get; set; }
+        public bool IsHidden { get; set; }
         public int CategoryId { get; set; }
     }
 }
